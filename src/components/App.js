4c884664843edb4d9a4e98311b273c6eca9d06f8
GreaterDogFr <benjaminherve76120@outlook.fr>
2023-11-09 15:05:59 +0100

    progression lundi après-midi

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Home from './Home/Home'
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+        <header className="App-header">
+          <img src={"../images/dwwm_1.png"} className="App-logo" alt="logo" />
+          <p>
+            TP React Développeur Web et Web Mobile
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Rechercher des films
+          </a>
+        </header>
+    </div>
+  );
+}
+
+export default App;
